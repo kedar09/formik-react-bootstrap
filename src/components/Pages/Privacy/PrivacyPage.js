a478a0b13132a9b7e41e226af2ecbd91254900c5
kedar09 <32976533+kedar09@users.noreply.github.com>
2020-08-12 16:09:14 +0530

    Add files via upload

---
+++ src/components/Pages/Privacy/PrivacyPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import "./privacy-page.css";
+
+class PrivacyPage extends React.Component {
+
+    render() {
+        return (
+            <div className="divPage">
+                <p>Privacy Page</p>
+            </div>
+        )
+    }
+}
+
+export default PrivacyPage;
